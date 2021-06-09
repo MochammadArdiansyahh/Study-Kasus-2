a529694b5b68c28dce0663c657f682a95a17a7e1
unknown <MochammadArdiansyahh>
2021-06-09 23:35:08 +0700

    berhasil

---
+++ src/study/kasus/pkg2/kedua.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package study.kasus.pkg2;
+
+/**
+ *
+ * @author Moch Ardiansyah
+ */
+public class kedua {
+    void tempattinggal(){
+        String namasiswa = "Mochammad Ardiansyah";
+        String alamat = "Jln Kertosari";
+        
+        System.out.println("Nama Siswa : "+namasiswa);
+        System.out.println("Alamat : "+alamat);
+    }
+}
